d1ade2540ef5ca339dc16e262fdb29135607ac56
YazanAlaiwah <eng.yazanalaiwah@gmail.com>
2020-02-04 23:28:44 +0200

    finsh lap06

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
 document.write(prompt('what is your name'))
+$(document).ready(function(){
+$("h1").click(function () {
+    // console.log($(this).text().replace($(this).text(), "hello everyone")) 
+    document.getElementsByTagName("h1")[0].innerHTML = "Thank You Goodbye♥☻"
+})
+})
